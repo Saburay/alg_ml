45e1464cc93336d9250e3bcc1e534ec4fca8ebc9
saburay32 <saburay32@gmail.com>
2024-02-29 10:11:53 +0300

    changed output

--- main.py
+++ main.py
@@ -35,20 +35,20 @@ class MyLineReg():
 
         n = len(self.y)
         x.insert(loc=0, column='ones', value=ones)#дополняем переданную матрицу фичей x единичным столбцом слева.
-        print(f'self_X_ones:\n{x}')
+        #print(f'self_X_ones:\n{x}')
         v_weights = np.ones(x.shape[1])#Определить сколько фичей передано и создать вектор весов,
                                # состоящий из одних единиц соответствующей длинны: т.е. количество фичей + 1.
-        print(f'self_v_wights:{len(v_weights)}{v_weights}')
+        #print(f'self_v_wights:{len(v_weights)}{v_weights}')
+        s = 'start'
         for i in range(self.n_iter):
-            s = 'start'
             pred_y = x.dot(v_weights)
             #print(f'pred_y:{pred_y}\n{type(pred_y)},self_y {type(self.y)})')
             mse = sum((pred_y-self.y)**2)/n
-            print(f'mse{mse}')
+            #print(f'mse{mse}')
             pred_min_y =(pred_y - self.y)
-            print(f'pred_min_y:    {pred_min_y}')
+            #print(f'pred_min_y:    {pred_min_y}')
             gr = ((2/n)*((pred_y-self.y).dot(x)))
-            print(f'gr------->{gr}')
+            #print(f'gr------->{gr}')
             v_weights = v_weights-self.learning_rate*gr
             self.weights = v_weights
             if verbose:
@@ -66,7 +66,7 @@ X.columns = [f'col_{col}' for col in X.columns]
 #print(X)
 
 zz = MyLineReg()
-zz.fit(X,y,True)
+zz.fit(X,y,verbose=True)
 
 #------------------------------------------------------
 # import numpy as np
