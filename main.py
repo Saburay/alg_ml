086d55ba7ff8c186af4518dfd739275c49f86182
saburay32 <saburay32@gmail.com>
2024-03-18 15:02:49 +0300

    adding regularization

--- main.py
+++ main.py
@@ -9,21 +9,32 @@ class MyLineReg():
     learning_rate — коэффициент скорости обучения градиентного спуска.
     По-умолчанию: 0.1
     weights — хранит веса модели
+    metric, который будет принимать одно из следующих значений:
+    - mae
+    - mse
+    - rmse
+    - mape
+    - r2
+    По умолчанию: None
+    reg – принимает одно из трех значений: l1, l2, elasticnet
+    По умолчанию: None
+    l1_coef – принимает значения от 0.0 до 1.0
+    По умолчанию: 0
+    l2_coef – принимает значения от 0.0 до 1.0
+    По умолчанию: 0
     '''
-    def __init__(self, n_iter=100, learning_rate=0.1, weights=None, metric=None):
+    def __init__(self, n_iter=100, learning_rate=0.1, weights=None, metric=None,reg = None, l1=0, l2=0):
         self.n_iter = n_iter
         self.learning_rate = learning_rate
         self.weights = weights
         self.metric = metric
+        self.l1 = l1
+        self.l2 = l2
+        self.reg = reg
         #self.score_dict = {'mse': mse, 'mae': mae, 'rmse': rmse, 'r2': r2, 'mape': mape}
 
     def __repr__(self):
         return f'MyLineReg class: n_iter={self.n_iter}, learning_rate={self.learning_rate}'
-        # if self.metric!=None:
-        #     return f'MyLineReg class: n_iter={self.n_iter}, learning_rate={self.learning_rate}, <{self.metric}>:'
-        # else:
-        #     return f'MyLineReg class: n_iter={self.n_iter}, learning_rate={self.learning_rate}'
-
 
     def fit(self, x, y, verbose=False):
         '''
@@ -38,29 +49,38 @@ class MyLineReg():
         self.y = pd.Series(y)
         self.verbose = verbose
 
-        self.mean_y = y.mean(axis=0)#среднее значение целевой переменной
+        self.mean_y = y.mean(axis=0)       #среднее значение целевой переменной
         self.best_score = None
 
         #n = len(self.y)
         n = x.shape[0]
-        x.insert(loc=0, column='ones', value=1)#дополняем переданную матрицу фичей x единичным столбцом слева.
-        v_weights = np.ones(x.shape[1])#Определить сколько фичей передано и создать вектор весов,
-                               # состоящий из одних единиц соответствующей длинны: т.е. количество фичей + 1.
+        x.insert(loc=0, column='ones', value=1) #дополняем переданную матрицу фичей x единичным столбцом слева.
+        v_weights = np.ones(x.shape[1])   #Определить сколько фичей передано и создать вектор весов,
+                                          # состоящий из одних единиц соответствующей длинны: т.е. количество фичей + 1.
         s = 'start'
 
         for i in range(self.n_iter+1):
             pred_y = x.dot(v_weights)
-            mse = sum((self.y - pred_y) ** 2) / n
-            gr = ((2/n)*((pred_y-self.y).dot(x)))#Вычисляем градиент
-            v_weights = v_weights-self.learning_rate*gr#шаг размером learning rate в противоположную от градиента сторону
+            mse = sum((self.y - pred_y)**2)/n                                            #функция потерь
+            lasso_mse = (sum((self.y - pred_y)**2)/n)+self.l1*(sum(abs(self.weights)))   #функция потерь  с L1 регуляризацией
+            ridge_mse = (sum((self.y - pred_y)**2)/n)+self.l2*(sum((self.weights)**2))   #функция потерь  с L2 регуляризацией
+            ElasticNet_mse = sum((self.y - pred_y)**2)/n#функция потерь  с ElasticNet регуляризацией
+
+            gr = ((2/n)*((pred_y-self.y).dot(x)))       #вычисляем градиент
+            #sgn_w = self.weights/abs(self.weights)
+            gr_lasso_mse = ((2/n)*((pred_y-self.y).dot(x)))+(self.l1*(self.weights/abs(self.weights))) #вычисляем градиент
+                                                                                                       #с L1 регуляризацией
+            gr_ridge_mse = ((2/n)*((pred_y-self.y).dot(x)))+(self.l2*(2*self.weights))                 #вычисляем градиент
+                                                                                                       #с L2 регуляризацией
+            v_weights = v_weights-self.learning_rate*gr #шаг размером learning rate в противоположную от градиента сторону
             self.weights = v_weights
 
-            mse = sum((self.y - pred_y) ** 2) / n
-            mae = sum(abs(self.y - pred_y))/ n
-            rmse = (sum((self.y - pred_y) ** 2) / n) ** 0.5
-            r2 = 1 - (sum((self.y - pred_y) ** 2) )/ (sum((self.y - self.mean_y) ** 2))
-            mape = (100 * (sum(abs((self.y - pred_y) / self.y)))/ n)
-            score_dict = {'mse': mse,'mae': mae,'rmse': rmse,'r2': r2,'mape': mape}
+            mse = sum((self.y - pred_y) ** 2)/n
+            mae = sum(abs(self.y - pred_y))/n
+            rmse = (sum((self.y - pred_y) ** 2)/n) ** 0.5
+            r2 = 1 - (sum((self.y - pred_y) ** 2))/(sum((self.y - self.mean_y) ** 2))
+            mape = (100 * (sum(abs((self.y - pred_y) / self.y)))/n)
+            score_dict = {'mse': mse,'mae': mae,'rmse': rmse,'r2': r2,'mape': mape} #словарь метрик
             if self.metric!=None:
                 self.best_score = score_dict[(self.metric).lower()]
             if verbose:
